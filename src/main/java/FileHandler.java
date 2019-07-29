ac1dd7dc8e7aa37148977f4c04ffedc5292174c1
vvinod <vvinod@tavisca.in>
2019-07-29 15:10:51 +0530

    Wrote read and write methods and successfully built

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -3,10 +3,22 @@ import java.io.FileNotFoundException;
 public class FileHandler {
     public static void main(String[] args) {
         writeFileContents();
+        readFileContents();
+    }
+    public static void readFileContents(){
+        FileReader fileReader=new FileReader();
+        try{
+            String contents=fileReader.readFile("DataFile.txt");
+            System.out.println(contents);
+        }
+        catch(FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
     }
     private static void writeFileContents() {
         FileWriter myFileWriter= new FileWriter();
-        String data = "Hello! How are you today?";
+        String data = "Hello! How are you toooday?";
         try {
             myFileWriter.writeFile("DataFile.txt", data);
         } catch (FileNotFoundException e) {
